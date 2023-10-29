271ba9650b16398dc4fc810f3907981dc7cff565
dcuoliveira <dan.cunhaa@gmail.com>
2023-10-29 19:49:04 -0300

    change args

--- src/run_pipeline.py
+++ src/run_pipeline.py
@@ -5,25 +5,20 @@ import os
 from forecast.forecast_funcs import run_forecast
 from metadata.etfs import etfs_large, etfs_small
 
-def get_args():
-    parser = argparse.ArgumentParser(description="Run forecast.")
-    parser.add_argument("--estimation_window", type=int, default=12 * 8)
-    parser.add_argument("--p", type=int, default=1)
-    parser.add_argument("--correl_window", type=int, default=1000)
-    parser.add_argument("--beta_threshold", type=float, default=0)
-    parser.add_argument("--fix_start", type=bool, default=True)
-    parser.add_argument("--incercept", type=bool, default=True)
-    parser.add_argument("--fs_method", type=str, default="lasso")
-    parser.add_argument("--cv_type", type=str, default="cv")
-    parser.add_argument("--inputs_path", type=str, default=os.path.join(os.path.dirname(__file__), "data", "inputs"))
-    
-    args = parser.parse_args()
-
-    return args
+parser = argparse.ArgumentParser(description="Run forecast.")
+parser.add_argument("--estimation_window", type=int, default=12 * 8)
+parser.add_argument("--p", type=int, default=1)
+parser.add_argument("--correl_window", type=int, default=1000)
+parser.add_argument("--beta_threshold", type=float, default=0)
+parser.add_argument("--fix_start", type=bool, default=True)
+parser.add_argument("--incercept", type=bool, default=True)
+parser.add_argument("--fs_method", type=str, default="lasso")
+parser.add_argument("--cv_type", type=str, default="cv")
+parser.add_argument("--inputs_path", type=str, default=os.path.join(os.path.dirname(__file__), "data", "inputs"))
 
 if __name__ == "__main__":
 
-    args = get_args()
+    args = parser.parse_args()
 
     data = pd.read_csv(os.path.join(args.inputs_path, 'etfs_macro_large.csv'))
     
