d0c7f64541af7e9f00d035872e1435bb15568f79
dcuoliveira <dan.cunhaa@gmail.com>
2023-11-02 20:18:18 -0300

    fix load pickle

--- src/utils/conn_data.py
+++ src/utils/conn_data.py
@@ -4,7 +4,7 @@ def save_pickle(obj, path):
     with open(path, 'wb') as f:
         pickle.dump(obj, f)
 
-def load_pickle(obj, path):
+def load_pickle(path):
     with open(path, 'rb') as f:
         obj = pickle.load(f)
     return obj
