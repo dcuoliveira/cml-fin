dd2c229ee129ab2f09d894c6e70ecb33c91fa4d7
dcuoliveira <dan.cunhaa@gmail.com>
2024-01-25 22:26:12 -0300

    change defautl

--- src/run_lasso_experiment.sh
+++ src/run_lasso_experiment.sh
@@ -1,2 +1,2 @@
-python run_pipeline.py --fs_method lasso1 --clustering_method None --n_clusters 0
-python run_pipeline.py --fs_method lasso2 --clustering_method None --n_clusters 0
+python run_pipeline.py --fs_method lasso1 --clustering_method no --n_clusters 0
+python run_pipeline.py --fs_method lasso2 --clustering_method no --n_clusters 0
