4434c329908d4da670f859e2aff905124a2394be
dcuoliveira <dan.cunhaa@gmail.com>
2024-01-28 10:41:01 -0300

    fix runners

--- src/run_pairwise_granger_experiment.sh
+++ src/run_pairwise_granger_experiment.sh
@@ -2,6 +2,6 @@ python run_pipeline.py --fs_method pairwise-granger --clustering_method no --n_c
 python run_pipeline.py --fs_method pairwise-granger --clustering_method kmeans --n_clusters 0
 python run_pipeline.py --fs_method pairwise-granger --clustering_method kmeans --n_clusters 5
 python run_pipeline.py --fs_method pairwise-granger --clustering_method kmeans --n_clusters 10
-# python run_pipeline.py --fs_method pairwise-granger --clustering_method rolling_kmeans --n_clusters 0
-# python run_pipeline.py --fs_method pairwise-granger --clustering_method rolling_kmeans --n_clusters 5
-# python run_pipeline.py --fs_method pairwise-granger --clustering_method rolling_kmeans --n_clusters 10
+python run_pipeline.py --fs_method pairwise-granger --clustering_method rolling_kmeans --n_clusters 0
+python run_pipeline.py --fs_method pairwise-granger --clustering_method rolling_kmeans --n_clusters 5
+python run_pipeline.py --fs_method pairwise-granger --clustering_method rolling_kmeans --n_clusters 10
