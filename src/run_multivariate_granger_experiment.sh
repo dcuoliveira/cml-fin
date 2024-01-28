73a467bf2bf1dfdc98b08871099f89401807e1a6
dcuoliveira <dan.cunhaa@gmail.com>
2024-01-28 08:56:55 -0300

    change comments

--- src/run_multivariate_granger_experiment.sh
+++ src/run_multivariate_granger_experiment.sh
@@ -1,7 +1,7 @@
-# python run_pipeline.py --fs_method multivariate-granger --clustering_method no --n_clusters 0
-# python run_pipeline.py --fs_method multivariate-granger --clustering_method kmeans --n_clusters 0
-# python run_pipeline.py --fs_method multivariate-granger --clustering_method kmeans --n_clusters 5
-# python run_pipeline.py --fs_method multivariate-granger --clustering_method kmeans --n_clusters 10
-python run_pipeline.py --fs_method multivariate-granger --clustering_method rolling_kmeans --n_clusters 0
-python run_pipeline.py --fs_method multivariate-granger --clustering_method rolling_kmeans --n_clusters 5
-python run_pipeline.py --fs_method multivariate-granger --clustering_method rolling_kmeans --n_clusters 10
+python run_pipeline.py --fs_method multivariate-granger --clustering_method no --n_clusters 0
+python run_pipeline.py --fs_method multivariate-granger --clustering_method kmeans --n_clusters 0
+python run_pipeline.py --fs_method multivariate-granger --clustering_method kmeans --n_clusters 5
+python run_pipeline.py --fs_method multivariate-granger --clustering_method kmeans --n_clusters 10
+# python run_pipeline.py --fs_method multivariate-granger --clustering_method rolling_kmeans --n_clusters 0
+# python run_pipeline.py --fs_method multivariate-granger --clustering_method rolling_kmeans --n_clusters 5
+# python run_pipeline.py --fs_method multivariate-granger --clustering_method rolling_kmeans --n_clusters 10
