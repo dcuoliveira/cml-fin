30c4ca6a506af5b7b2ca7e0115a6488c0e9d3085
dcuoliveira <dan.cunhaa@gmail.com>
2024-03-10 09:07:35 -0300

    commnet non relevant experiments

--- src/run_var_lingam_experiment.sh
+++ src/run_var_lingam_experiment.sh
@@ -1,6 +1,6 @@
-python run_pipeline.py --fs_method var-lingam --clustering_method kmeans --n_clusters 0
-python run_pipeline.py --fs_method var-lingam --clustering_method kmeans --n_clusters 5
-python run_pipeline.py --fs_method var-lingam --clustering_method kmeans --n_clusters 10
+# python run_pipeline.py --fs_method var-lingam --clustering_method kmeans --n_clusters 0
+# python run_pipeline.py --fs_method var-lingam --clustering_method kmeans --n_clusters 5
+# python run_pipeline.py --fs_method var-lingam --clustering_method kmeans --n_clusters 10
 python run_pipeline.py --fs_method var-lingam --clustering_method rolling_kmeans --n_clusters 0
 python run_pipeline.py --fs_method var-lingam --clustering_method rolling_kmeans --n_clusters 5
 python run_pipeline.py --fs_method var-lingam --clustering_method rolling_kmeans --n_clusters 10
