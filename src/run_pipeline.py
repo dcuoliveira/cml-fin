3db41e5ab7c52b82a9fa81ade2a8ed957be3ae09
dcuoliveira <dan.cunhaa@gmail.com>
2024-04-02 06:44:31 -0300

    add default

--- src/run_pipeline.py
+++ src/run_pipeline.py
@@ -17,7 +17,7 @@ parser.add_argument("--pval_threshold", type=float, default=0.05)
 parser.add_argument("--fix_start", type=str, default=True)
 parser.add_argument("--incercept", type=str, default=True)
 parser.add_argument("--fs_method", type=str, default="var-lingam") # available - var-lingam, dynotears, lasso, pairwise-granger, multivariate-granger
-parser.add_argument("--opt_k_method", type=str, default="eigen") # available - eigen, sillhouette, elbow
+parser.add_argument("--opt_k_method", type=str, default="eigen") # available - eigen, sillhouette, elbow, no
 parser.add_argument("--clustering_method", type=str, default="rolling_kmeans") # available - kmean, rolling_kmeans, spectral, rolling_spectral, no
 parser.add_argument("--n_clusters", type=int, default=0) # 0 for auto select k
 parser.add_argument("--data_name", type=str, default="etfs_macro_large")
