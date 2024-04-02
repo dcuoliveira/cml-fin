c817e688006dc7f2c31df56e85dedac54527d3a3
dcuoliveira <dan.cunhaa@gmail.com>
2024-04-02 06:53:53 -0300

    add param

--- src/run_lasso_experiment.sh
+++ src/run_lasso_experiment.sh
@@ -1,2 +1,2 @@
-python run_pipeline.py --fs_method lasso1 --clustering_method no --n_clusters 0
-python run_pipeline.py --fs_method lasso2 --clustering_method no --n_clusters 0
+python run_pipeline.py --fs_method lasso1 --clustering_method no --opt_k_method no --n_clusters 0
+python run_pipeline.py --fs_method lasso2 --clustering_method no --opt_k_method no --n_clusters 0
