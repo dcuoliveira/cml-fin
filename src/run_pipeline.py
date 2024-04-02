ac8d35ad500db97fc39751f12671ba797e377b50
dcuoliveira <dan.cunhaa@gmail.com>
2024-04-02 06:32:37 -0300

    add defautls

--- src/run_pipeline.py
+++ src/run_pipeline.py
@@ -16,10 +16,10 @@ parser.add_argument("--beta_threshold", type=float, default=0.4)
 parser.add_argument("--pval_threshold", type=float, default=0.05)
 parser.add_argument("--fix_start", type=str, default=True)
 parser.add_argument("--incercept", type=str, default=True)
-parser.add_argument("--fs_method", type=str, default="var-lingam")
-parser.add_argument("--opt_k_method", type=str, default="eigen")
-parser.add_argument("--clustering_method", type=str, default="rolling_kmeans")
-parser.add_argument("--n_clusters", type=int, default=0)
+parser.add_argument("--fs_method", type=str, default="var-lingam") # available - var-lingam, dynotears, lasso, pairwise-granger, multivariate-granger
+parser.add_argument("--opt_k_method", type=str, default="eigen") # available - eigen, sillhouette, elbow
+parser.add_argument("--clustering_method", type=str, default="rolling_kmeans") # available - kmean, rolling_kmeans, spectral, rolling_spectral, no
+parser.add_argument("--n_clusters", type=int, default=0) # 0 for auto select k
 parser.add_argument("--data_name", type=str, default="etfs_macro_large")
 parser.add_argument("--inputs_path", type=str, default=os.path.join(os.path.dirname(__file__), "data", "inputs"))
 parser.add_argument("--outputs_path", type=str, default=os.path.join(os.path.dirname(__file__), "data", "outputs"))
