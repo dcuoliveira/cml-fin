5fb4869e37be736f9c174a48a3bce2634d4af0c5
dcuoliveira <dan.cunhaa@gmail.com>
2024-04-07 14:12:32 -0300

    add more lasso models

--- src/run_lasso_experiment.sh
+++ src/run_lasso_experiment.sh
@@ -1,2 +1,22 @@
-python run_pipeline.py --fs_method lasso1 --clustering_method no --opt_k_method no --n_clusters 0 --intra_cluster_selection rank
-python run_pipeline.py --fs_method lasso2 --clustering_method no --opt_k_method no --n_clusters 0 --intra_cluster_selection rank
+# python run_pipeline.py --fs_method lasso1 --clustering_method no --opt_k_method no --n_clusters 0 --intra_cluster_selection rank
+# python run_pipeline.py --fs_method lasso2 --clustering_method no --opt_k_method no --n_clusters 0 --intra_cluster_selection rank
+python run_pipeline.py --fs_method lasso1 --clustering_method rolling_kmeans --opt_k_method eigen --n_clusters 0 --intra_cluster_selection rank
+python run_pipeline.py --fs_method lasso1 --clustering_method rolling_spectral --opt_k_method eigen --n_clusters 0 --intra_cluster_selection rank
+python run_pipeline.py --fs_method lasso1 --clustering_method rolling_kmeans --opt_k_method no --n_clusters 5 --intra_cluster_selection rank
+python run_pipeline.py --fs_method lasso1 --clustering_method rolling_kmeans --opt_k_method no --n_clusters 10 --intra_cluster_selection rank
+python run_pipeline.py --fs_method lasso1 --clustering_method rolling_spectral --opt_k_method no --n_clusters 5 --intra_cluster_selection rank
+python run_pipeline.py --fs_method lasso1 --clustering_method rolling_spectral --opt_k_method no --n_clusters 10 --intra_cluster_selection rank
+python run_pipeline.py --fs_method lasso1 --clustering_method rolling_kmeans --opt_k_method no --n_clusters 5 --intra_cluster_selection pca
+python run_pipeline.py --fs_method lasso1 --clustering_method rolling_kmeans --opt_k_method no --n_clusters 10 --intra_cluster_selection pca
+python run_pipeline.py --fs_method lasso1 --clustering_method rolling_spectral --opt_k_method no --n_clusters 5 --intra_cluster_selection pca
+python run_pipeline.py --fs_method lasso1 --clustering_method rolling_spectral --opt_k_method no --n_clusters 10 --intra_cluster_selection pca
+python run_pipeline.py --fs_method lasso2 --clustering_method rolling_kmeans --opt_k_method eigen --n_clusters 0 --intra_cluster_selection rank
+python run_pipeline.py --fs_method lasso2 --clustering_method rolling_spectral --opt_k_method eigen --n_clusters 0 --intra_cluster_selection rank
+python run_pipeline.py --fs_method lasso2 --clustering_method rolling_kmeans --opt_k_method no --n_clusters 5 --intra_cluster_selection rank
+python run_pipeline.py --fs_method lasso2 --clustering_method rolling_kmeans --opt_k_method no --n_clusters 10 --intra_cluster_selection rank
+python run_pipeline.py --fs_method lasso2 --clustering_method rolling_spectral --opt_k_method no --n_clusters 5 --intra_cluster_selection rank
+python run_pipeline.py --fs_method lasso2 --clustering_method rolling_spectral --opt_k_method no --n_clusters 10 --intra_cluster_selection rank
+python run_pipeline.py --fs_method lasso2 --clustering_method rolling_kmeans --opt_k_method no --n_clusters 5 --intra_cluster_selection pca
+python run_pipeline.py --fs_method lasso2 --clustering_method rolling_kmeans --opt_k_method no --n_clusters 10 --intra_cluster_selection pca
+python run_pipeline.py --fs_method lasso2 --clustering_method rolling_spectral --opt_k_method no --n_clusters 5 --intra_cluster_selection pca
+python run_pipeline.py --fs_method lasso2 --clustering_method rolling_spectral --opt_k_method no --n_clusters 10 --intra_cluster_selection pca
