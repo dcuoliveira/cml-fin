7f1c1858e31f0a7d08a50667ddcb6216534a797a
dcuoliveira <dan.cunhaa@gmail.com>
2024-06-08 11:16:43 -0300

    add pcmci run

---
+++ src/run_pcmci_experiment.sh
@@ -0,0 +1,9 @@
+python run_pipeline.py --fs_method pcmci --clustering_method rolling_kmeans --opt_k_method no --n_clusters 5 --intra_cluster_selection rank
+python run_pipeline.py --fs_method pcmci --clustering_method rolling_kmeans --opt_k_method no --n_clusters 5 --intra_cluster_selection pca
+python run_pipeline.py --fs_method pcmci --clustering_method rolling_kmeans --opt_k_method no --n_clusters 10 --intra_cluster_selection rank
+python run_pipeline.py --fs_method pcmci --clustering_method rolling_kmeans --opt_k_method no --n_clusters 10 --intra_cluster_selection pca
+python run_pipeline.py --fs_method pcmci --clustering_method rolling_spectral --opt_k_method no --n_clusters 5 --intra_cluster_selection rank
+python run_pipeline.py --fs_method pcmci --clustering_method rolling_spectral --opt_k_method no --n_clusters 5 --intra_cluster_selection pca
+python run_pipeline.py --fs_method pcmci --clustering_method rolling_spectral --opt_k_method no --n_clusters 10 --intra_cluster_selection rank
+python run_pipeline.py --fs_method pcmci --clustering_method rolling_spectral --opt_k_method no --n_clusters 10 --intra_cluster_selection pca
+python run_pipeline.py --fs_method pcmci --clustering_method no --opt_k_method no --n_clusters 0 --intra_cluster_selection no
