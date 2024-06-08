c89870cb0795c2a484b8263e5cfb56bd974a4993
dcuoliveira <dan.cunhaa@gmail.com>
2024-06-08 08:04:59 -0300

    update runners

--- src/run_var_lingam_experiment.sh
+++ src/run_var_lingam_experiment.sh
@@ -1,10 +1,9 @@
-python run_pipeline.py --fs_method var-lingam --clustering_method rolling_kmeans --opt_k_method eigen --n_clusters 0 --intra_cluster_selection rank
-python run_pipeline.py --fs_method var-lingam --clustering_method rolling_kmeans --opt_k_method sillhouette --n_clusters 0 --intra_cluster_selection rank
-python run_pipeline.py --fs_method var-lingam --clustering_method rolling_kmeans --opt_k_method elbow --n_clusters 0 --intra_cluster_selection rank
-python run_pipeline.py --fs_method var-lingam --clustering_method rolling_spectral --opt_k_method eigen --n_clusters 0 --intra_cluster_selection rank
-python run_pipeline.py --fs_method var-lingam --clustering_method rolling_spectral --opt_k_method sillhouette --n_clusters 0 --intra_cluster_selection rank
-python run_pipeline.py --fs_method var-lingam --clustering_method rolling_spectral --opt_k_method elbow --n_clusters 0 --intra_cluster_selection rank
 python run_pipeline.py --fs_method var-lingam --clustering_method rolling_kmeans --opt_k_method no --n_clusters 5 --intra_cluster_selection rank
+python run_pipeline.py --fs_method var-lingam --clustering_method rolling_kmeans --opt_k_method no --n_clusters 5 --intra_cluster_selection pca
 python run_pipeline.py --fs_method var-lingam --clustering_method rolling_kmeans --opt_k_method no --n_clusters 10 --intra_cluster_selection rank
+python run_pipeline.py --fs_method var-lingam --clustering_method rolling_kmeans --opt_k_method no --n_clusters 10 --intra_cluster_selection pca
 python run_pipeline.py --fs_method var-lingam --clustering_method rolling_spectral --opt_k_method no --n_clusters 5 --intra_cluster_selection rank
+python run_pipeline.py --fs_method var-lingam --clustering_method rolling_spectral --opt_k_method no --n_clusters 5 --intra_cluster_selection pca
 python run_pipeline.py --fs_method var-lingam --clustering_method rolling_spectral --opt_k_method no --n_clusters 10 --intra_cluster_selection rank
+python run_pipeline.py --fs_method var-lingam --clustering_method rolling_spectral --opt_k_method no --n_clusters 10 --intra_cluster_selection pca
+python run_pipeline.py --fs_method var-lingam --clustering_method no --opt_k_method no --n_clusters 0 --intra_cluster_selection no
