7721e82d47d9a6c4baf78907b1541ea2208a4f11
dcuoliveira <dan.cunhaa@gmail.com>
2024-06-07 21:43:29 -0300

    add lags

--- src/utils/parsers.py
+++ src/utils/parsers.py
@@ -1,3 +1,30 @@
+import pandas as pd
+
+def add_and_keep_lags_only(data, lags):
+    """
+    Add lags to a dataframe
+
+    Parameters
+    ----------
+    data : pd.DataFrame
+        Dataframe to add lags to
+    lags : int
+        Number of lags to add
+
+    Returns
+    -------
+    pd.DataFrame
+        Dataframe with lags added
+    """
+
+    for c in data.columns:
+        for lag in range(1, lags + 1):
+            new_column = data[c].shift(lag)
+            data = pd.concat([data, pd.DataFrame(new_column.values, columns=["{}(t-{})".format(c, lag)], index=data.index)], axis=1)
+        
+        data = data.drop(c, axis=1)
+    
+    return data
 
 def str_2_bool(val):
 
