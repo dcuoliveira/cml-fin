6ec676ec643199d7d583ce72885f4c4a1d097616
dcuoliveira <dan.cunhaa@gmail.com>
2024-10-31 13:28:41 -0300

    add svm to wrappers

--- src/models/ModelClasses.py
+++ src/models/ModelClasses.py
@@ -1,9 +1,10 @@
 import os
 import pandas as pd
 import numpy as np
-from scipy.stats import randint as sp_randint
+from scipy.stats import randint as sp_randint, uniform
 from sklearn.linear_model import Lasso, LinearRegression
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.svm import SVR
 
 class LinearRegressionWrapper():
     def __init__(self, model_params={'fit_intercept': True}):
@@ -46,16 +47,41 @@ class LassoWrapper():
 
 class RandomForestWrapper():
     def __init__(self, model_params=None):
-        self.model_name = "random_forest"
+        self.model_name = "rf"
         self.search_type = 'random'
-        self.param_grid = {'model__max_features': ['auto', 'sqrt', 'log2'],
-                           'model__min_samples_split': sp_randint(2, 31),
-                           'model__n_estimators': sp_randint(2, 301),
-                           'model__max_depth': sp_randint(2, 20)}
+        # self.param_grid = {'model__max_features': ['auto', 'sqrt', 'log2'],
+        #                    'model__min_samples_split': sp_randint(2, 31),
+        #                    'model__n_estimators': sp_randint(2, 301),
+        #                    'model__max_depth': sp_randint(2, 20)}
+
+        self.param_grid = {
+            'model__max_depth': sp_randint(1, 7),
+            'model__n_estimators': [300],
+            'model__max_features': [3, 5, 10, 20, 30, 50]
+        }
+        
+
         if model_params is None:
             self.ModelClass = RandomForestRegressor()
         else:
             self.ModelClass = RandomForestRegressor(**model_params)
+
+class SVMWrapper():
+    def __init__(self, model_params=None):
+        self.model_name = "svm"
+        self.search_type = 'random'
+
+        self.param_grid = {
+            'model__C': uniform(0.1, 100),  # Regularization parameter, typically 0.1 to 100
+            'model__kernel': ['linear', 'poly', 'rbf'],  # Common SVM kernels
+            'model__degree': [2, 3, 4, 5],  # Degree for 'poly' kernel
+            'model__gamma': ['scale', 'auto']  # Gamma parameter for 'rbf' kernel
+        }
+
+        if model_params is None:
+            self.ModelClass = SVR()
+        else:
+            self.ModelClass = SVR(**model_params)
 
 DEBUG = False
 
