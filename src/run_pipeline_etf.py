f04aacb8e4f8637078a95cfbdb6c1c51de5c4fd1
dcuoliveira <dan.cunhaa@gmail.com>
2024-11-02 09:18:50 -0300

    fix benchmark names

--- src/run_pipeline_etf.py
+++ src/run_pipeline_etf.py
@@ -16,7 +16,7 @@ parser.add_argument("--beta_threshold", type=float, default=0.4)
 parser.add_argument("--pval_threshold", type=float, default=0.05)
 parser.add_argument("--fix_start", type=str, default=True)
 parser.add_argument("--incercept", type=str, default=True)
-parser.add_argument("--fs_method", type=str, default="seqICP", choices=["var-lingam", "dynotears", "lasso1", "lasso2", "pairwise-granger", "multivariate-granger", "sfstscv-lin", "sfstscv-rf", "rfetscv-lin", "rfetscv-rf", "pcmci", "seqICP"])
+parser.add_argument("--fs_method", type=str, default="sfscv-forward-lin", choices=["var-lingam", "dynotears", "lasso1", "lasso2", "pairwise-granger", "multivariate-granger", 'sfscv-backward-svm', "sfscv-forward-svm", "sfscv-backward-rf", "sfscv-forward-rf", "sfscv-backward-lin", "sfscv-forward-lin", 'sfstscv-backward-svm', "sfstscv-forward-svm", "sfstscv-backward-rf", "sfstscv-forward-rf", "sfstscv-backward-lin", "sfstscv-forward-lin", "pcmci", "seqICP"])
 parser.add_argument("--opt_k_method", type=str, default="no", choices=["eigen", "sillhouette", "elbow", "no"])
 parser.add_argument("--clustering_method", type=str, default="no", choices=["kmeans", "rolling_kmeans", "spectral", "rolling_spectral", "no"])
 parser.add_argument("--n_clusters", type=int, default=0) # 0 for auto select k
