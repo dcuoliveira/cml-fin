e05f57f723bbd07f00b0b36231cc50f83d96eb15
dcuoliveira <dan.cunhaa@gmail.com>
2025-01-22 14:32:56 -0300

    change dynotears runner

--- src/run_dynotears_experiment.sh
+++ src/run_dynotears_experiment.sh
@@ -1,9 +1,10 @@
-python run_pipeline.py --fs_method dynotears --clustering_method rolling_kmeans --opt_k_method no --n_clusters 5 --intra_cluster_selection rank
-python run_pipeline.py --fs_method dynotears --clustering_method rolling_kmeans --opt_k_method no --n_clusters 5 --intra_cluster_selection pca
-python run_pipeline.py --fs_method dynotears --clustering_method rolling_kmeans --opt_k_method no --n_clusters 10 --intra_cluster_selection rank
-python run_pipeline.py --fs_method dynotears --clustering_method rolling_kmeans --opt_k_method no --n_clusters 10 --intra_cluster_selection pca
-python run_pipeline.py --fs_method dynotears --clustering_method rolling_spectral --opt_k_method no --n_clusters 5 --intra_cluster_selection rank
-python run_pipeline.py --fs_method dynotears --clustering_method rolling_spectral --opt_k_method no --n_clusters 5 --intra_cluster_selection pca
-python run_pipeline.py --fs_method dynotears --clustering_method rolling_spectral --opt_k_method no --n_clusters 10 --intra_cluster_selection rank
-python run_pipeline.py --fs_method dynotears --clustering_method rolling_spectral --opt_k_method no --n_clusters 10 --intra_cluster_selection pca
-python run_pipeline.py --fs_method dynotears --clustering_method no --opt_k_method no --n_clusters 0 --intra_cluster_selection no
+ python run_pipeline_etf.py --fs_method dynotears --opt_k_method no --clustering_method no --n_clusters 0 --intra_cluster_selection no --target SPY --data_name etfs_macro_large --p 1 --apply_lasso True
+python run_pipeline_etf.py --fs_method dynotears --opt_k_method no --clustering_method no --n_clusters 0 --intra_cluster_selection no --target XLI --data_name etfs_macro_large --p 1 --apply_lasso True
+python run_pipeline_etf.py --fs_method dynotears --opt_k_method no --clustering_method no --n_clusters 0 --intra_cluster_selection no --target XLE --data_name etfs_macro_large --p 1 --apply_lasso True
+python run_pipeline_etf.py --fs_method dynotears --opt_k_method no --clustering_method no --n_clusters 0 --intra_cluster_selection no --target XLK --data_name etfs_macro_large --p 1 --apply_lasso True
+python run_pipeline_etf.py --fs_method dynotears --opt_k_method no --clustering_method no --n_clusters 0 --intra_cluster_selection no --target XLV --data_name etfs_macro_large --p 1 --apply_lasso True
+python run_pipeline_etf.py --fs_method dynotears --opt_k_method no --clustering_method no --n_clusters 0 --intra_cluster_selection no --target XLU --data_name etfs_macro_large --p 1 --apply_lasso True
+python run_pipeline_etf.py --fs_method dynotears --opt_k_method no --clustering_method no --n_clusters 0 --intra_cluster_selection no --target XLF --data_name etfs_macro_large --p 1 --apply_lasso True
+python run_pipeline_etf.py --fs_method dynotears --opt_k_method no --clustering_method no --n_clusters 0 --intra_cluster_selection no --target XLY --data_name etfs_macro_large --p 1 --apply_lasso True
+python run_pipeline_etf.py --fs_method dynotears --opt_k_method no --clustering_method no --n_clusters 0 --intra_cluster_selection no --target XLP --data_name etfs_macro_large --p 1 --apply_lasso True
+python run_pipeline_etf.py --fs_method dynotears --opt_k_method no --clustering_method no --n_clusters 0 --intra_cluster_selection no --target XLB --data_name etfs_macro_large --p 1 --apply_lasso True
